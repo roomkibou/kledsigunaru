1e26d266f0c52985fe132a0b31a17ffca309438a
roomkibou <113073945+roomkibou@users.noreply.github.com>
2022-09-08 12:02:25 +0900

    MakeCodeプロジェクトの初期ファイル

---
+++ main.ts
@@ -0,0 +1,11 @@
+basic.forever(function () {
+    pins.digitalWritePin(DigitalPin.P0, 1)
+    basic.pause(2000)
+    pins.digitalWritePin(DigitalPin.P0, 0)
+    pins.digitalWritePin(DigitalPin.P2, 1)
+    basic.pause(2000)
+    pins.digitalWritePin(DigitalPin.P2, 0)
+    pins.digitalWritePin(DigitalPin.P1, 1)
+    basic.pause(2000)
+    pins.digitalWritePin(DigitalPin.P1, 0)
+})
